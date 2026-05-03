ed338b4fa82ed7a1c46a22efd9acdf65b3bd1197
María Reyes <marupaula.2004@gmail.com>
2026-05-03 17:04:25 -0500

    t

--- practice.py
+++ practice.py
@@ -4,9 +4,16 @@ def print_songs(song_list):
     for song in song_list:
         print(song)
 
-songs = [
-    Song("tv off", "Kendrick Lamar", 3.7),
-    Song("Alright", "Kendrick Lamar", 3.5)
-]
+songs = []
+
+n = int(input("How many songs do you want to enter? "))
+
+for _ in range(n):
+    title = input("Enter song title: ")
+    artist = input("Enter artist name: ")
+    duration = float(input("Enter duration: "))
+    
+    song = Song(title, artist, duration)
+    songs.append(song)
 
 print_songs(songs)
