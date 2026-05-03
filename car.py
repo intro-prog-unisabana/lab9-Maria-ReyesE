087c37ff1313875228c49f8d140ba317706a0c94
María Reyes <marupaula.2004@gmail.com>
2026-05-03 17:25:46 -0500

    t

--- car.py
+++ car.py
@@ -1,2 +1,17 @@
 # Write your class definition here!
+class Car:
+    def __init__(self, car_id, brand, year, color, mileage):
+        self.car_id = car_id
+        self.brand = brand
+        self.year = year
+        self.color = color
+        self.mileage = 0.0
 
+    def change_color(self, new_color):
+        self.color = new_color
+
+    def drive(self, miles):
+        self.mileage += miles
+
+    def __str__(self):
+        return f"{self.car_id} - {self.year} {self.color} {self.brand} with {self.mileage} miles"
