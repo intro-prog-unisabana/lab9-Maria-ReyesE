fd149c05b688c0a2358b67c306f41998aa1ed945
María Reyes <marupaula.2004@gmail.com>
2026-05-03 17:32:55 -0500

    t

--- practice.py
+++ practice.py
@@ -3,17 +3,3 @@ from song import Song
 def print_songs(song_list):
     for song in song_list:
         print(song)
-
-songs = []
-
-n = int(input("How many songs do you want to enter? "))
-
-for _ in range(n):
-    title = input("Enter song title: ")
-    artist = input("Enter artist name: ")
-    duration = float(input("Enter duration: "))
-    
-    song = Song(title, artist, duration)
-    songs.append(song)
-
-print_songs(songs)
